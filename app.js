905c7de1497fb069a8df45c597ce14972a8767ad
Developer-Prakash-Karuppasamy <158708116+Developer-Prakash-Karuppasamy@users.noreply.github.com>
2024-02-03 13:12:17 +0530

    Add files via upload
    for device detect using express-device package

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+var device = require('express-device');
+app.use(device.capture());
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => {
+  console.log(`Hi to ${req.device.type.toUpperCase()} User`);
+  res.type('html').send(html)}
+);
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
